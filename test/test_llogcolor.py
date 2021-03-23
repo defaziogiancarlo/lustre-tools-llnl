ebbc26bc482453c57ac8ca7363a63363db416a40
Gian-Carlo DeFazio <defazio1@llnl.gov>
2021-03-23 16:10:51 -0700

    got tests to run with unittest

--- test/test_llogcolor.py
+++ test/test_llogcolor.py
@@ -24,38 +24,35 @@ import unittest
 from importlib.util import spec_from_loader, module_from_spec
 from importlib.machinery import SourceFileLoader
 
-path_to_llogcolor = pathlib.Path('../scripts/llogcolor').resolve()
+path_to_llogcolor = str(pathlib.Path('../scripts/llogcolor').resolve())
 spec = spec_from_loader(
     'llogcolor',
     SourceFileLoader('llogcolor', path_to_llogcolor)
 )
-foobar = module_from_spec(spec)
-spec.loader.exec_module(foobar)
+llogcolor = module_from_spec(spec)
+spec.loader.exec_module(llogcolor)
+
+
 
 
 #### paths to input logs ###
-
 # small and simple log file, all of the same thread ID
-in_simple = 'color_samples/sample_logs'
-
+in_simple = 'llogcolor_files/sample_logs'
 
 ### paths to output logs ###
-
 # [-P, -C, in_simple]
-out_P_C_simple = 'color_samples/sample_logs'
+out_P_C_simple = 'llogcolor_files/sample_logs'
 # [-C, in_simple]
-out_P_simple = 'color_samples/out_simple_color'
+out_P_simple = 'llogcolor_files/out_simple_color'
 
 
 def cmp_stdout_to_ref(test_args, reference):
-    '''
-    Compare stdout to reference file.
+    '''Compare stdout to reference file.
     run llogcolor with test_args and write
     the output to a temporary file.
     Then compare that file to a reference file.
     Return True if the files match and False otherwise.
     '''
-
     # stdout will be changed to the temporary file
     # for when llogcolor outputs stuff
     # so save the real stdout now
@@ -69,7 +66,6 @@ def cmp_stdout_to_ref(test_args, reference):
 
     # run llogcolor with test_args and save the return value
     ret_val = llogcolor.main(args=test_args)
-
 
     # put stdout back to normal
     sys.stdout = real_stdout
@@ -91,15 +87,15 @@ class TestTextOutput(unittest.TestCase):
     that output to a reference file.
     '''
 
-    def test_P_C_simple():
+    def test_P_C_simple(self):
         '''read a log file in, and just send it unmodified to stdout'''
         # no pager, no color
         test_args = ['-P', '-C', in_simple]
         reference = out_P_C_simple
 
-        assert cmp_stdout_to_ref(test_args, reference)
+        self.assertTrue(cmp_stdout_to_ref(test_args, reference))
 
-    def test_P_simple():
+    def test_P_simple(self):
         '''read a log file in, and send the text to stdout.
         In this case the text includes the terminal color codes
         In this case all the lines will have the same color code.
@@ -108,17 +104,7 @@ class TestTextOutput(unittest.TestCase):
         test_args = ['-P', in_simple]
         reference = out_P_simple
 
-        assert cmp_stdout_to_ref(test_args, reference)
+        self.assertTrue(cmp_stdout_to_ref(test_args, reference))
 
-#def no_files():
-
-
-
-# read in several files
-# read in 0 files
-# check that highlighting a specific pid works
-# read files with lines that aren't matched by the log file regex
-# I'm not really sure if it's supposed to be writing to files, but check that
-# and if it does, check that the files are created/deleted correctly
-# give it file names that don't exist or have bad permissions
-# can I test the case where sys.stout is not a TTY
+if __name__ == '__main__':
+    unittest.main()
